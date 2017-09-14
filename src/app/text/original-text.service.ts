7f7af6b59669c55ce3e68aa4ed570d3953b2077e
Ivan de la Beldad <ivandelabeldad@gmail.com>
2017-09-14 02:34:49 +0200

    Fixed text dimension

--- src/app/text/original-text.service.ts
+++ src/app/text/original-text.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 import { TextService } from './text.service';
 
 @Injectable()
-export class OriginalTextService extends TextService{
+export class OriginalTextService extends TextService {
 }
--- src/app/text/translated-text.service.ts
+++ src/app/text/translated-text.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 import { TextService } from './text.service';
 
 @Injectable()
-export class TranslatedTextService extends TextService{
+export class TranslatedTextService extends TextService {
 }
